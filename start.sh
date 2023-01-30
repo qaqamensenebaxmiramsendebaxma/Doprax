04eb982a502c63a153ba24f7d96f1a3fe2bb3726
qaqamensenebaxmiramsendebaxma <121292218+qaqamensenebaxmiramsendebaxma@users.noreply.github.com>
2023-01-30 17:34:39 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/qaqamensenebaxmiramsendebaxma/team ok && cd ok && pip3 install -U -r requirements.txt
+bash start
